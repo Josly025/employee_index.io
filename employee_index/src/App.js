9300ea74cdef97cb25515e524fe3c6dc772a1ad3
Grantjos12 <grantjoslyn@gmail.com>
2020-11-19 22:40:37 -0600

    added search feature and deleted old Employees component for functions

--- employee_index/src/App.js
+++ employee_index/src/App.js
@@ -1,13 +1,143 @@
 import React, { Component } from "react";
 import SearchForm from "./components/SearchForm/SearchForm";
-import Employees from "./components/Employees/Employees";
+import API from "./components/utils/API.js";
 
-function App() {
-  return (
-    <div>
-      <SearchForm />
-      <Employees />;
-    </div>
-  );
+class App extends Component {
+  //States
+  state = {
+    search: "",
+    employees: [],
+  };
+
+  componentDidMount() {
+    API.search()
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          employees: res.data.results.map((e) => ({
+            firstName: e.name.first,
+            lastName: e.name.last,
+            picture: e.picture.thumbnail,
+            email: e.email,
+            phone: e.phone,
+            dob: e.dob.age,
+          })),
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+
+  // //API Search Call from API.js
+  // searchEmployees = (query) => {
+  //   API.search(query).then((res) => {
+  //     this.setState({ employees: res.data.results });
+  //     console.log(res.data.results);
+  //   });
+  // };s
+
+  //Refresh page after search
+  refreshPage = () => {
+    console.log("clicked");
+    window.location.reload();
+  };
+
+  searchEmployees = (filter) => {
+    console.log("Search", filter);
+    const filteredList = this.state.employees.filter((employee) => {
+      // merge data together, then check to see if employee exists
+      let values = Object.values(employee).join("").toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    // Update the employee list with the filtered value
+    this.setState({ employees: filteredList });
+  };
+
+  //handling search input
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+    console.log(value);
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Button Clicked", this.state.search, e);
+    this.searchEmployees(this.state.search);
+  };
+
+  //Render
+  render() {
+    return (
+      <div>
+        <SearchForm />
+        <div className="container">
+          <div className="row"></div>
+          <div className="input-group mb-3 col-md">
+            <div className="input-group-prepend">
+              <button
+                className="btn btn-outline-dark"
+                type="button"
+                id="button-addon1"
+                onClick={this.refreshPage}
+              >
+                All Employees
+              </button>
+              <button
+                className="btn btn-outline-dark"
+                type="button"
+                id="button-addon1"
+                onClick={this.handleFormSubmit}
+              >
+                Search
+              </button>
+            </div>
+            <input
+              onChange={this.handleInputChange}
+              value={this.value}
+              name="search"
+              type="text"
+              className="form-control"
+              placeholder="Enter search information"
+              aria-label="Example text with button addon"
+              aria-describedby="button-addon1"
+            ></input>
+          </div>
+        </div>
+
+        <div className="container">
+          <table className="table table-hover">
+            <thead className="bg-dark text-warning">
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>DOB</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[...this.state.employees].map((data) => (
+                <tr>
+                  <td>
+                    <img alt="img" src={data.picture}></img>
+                  </td>
+                  <td>
+                    {data.firstName} {data.lastName}
+                  </td>
+                  <td>{data.email}</td>
+                  <td>{data.phone}</td>
+                  <td>{data.dob}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
+
 export default App;
--- employee_index/src/components/SearchForm/SearchForm.js
+++ employee_index/src/components/SearchForm/SearchForm.js
@@ -5,38 +5,12 @@ function SearchForm(props) {
     <div>
       <div className="container-fluid ">
         <div className="jumbotron bg-warning text-center border border-dark">
-          <h1>Employee Directory</h1>
+          <h1>Employee Index</h1>
           <p>
             {" "}
-            | Search by Employee Name or Phone Number to view their contact |{" "}
+            | Search by Employee Name, Phone Number, or Age to view their
+            Contact Information |{" "}
           </p>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row"></div>
-
-        <div className="input-group mb-3 col-md">
-          <div className="input-group-prepend">
-            <button
-              className="btn btn-outline-dark"
-              type="button"
-              id="button-addon1"
-              onClick={props.handleFormSubmit}
-            >
-              Search
-            </button>
-          </div>
-          <input
-            onChange={props.handleInputChange}
-            value={props.search}
-            name={props.name}
-            id="search"
-            type="text"
-            className="form-control"
-            placeholder="Enter search information"
-            aria-label="Example text with button addon"
-            aria-describedby="button-addon1"
-          ></input>
         </div>
       </div>
     </div>
